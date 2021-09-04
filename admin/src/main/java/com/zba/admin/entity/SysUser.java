d4ff6dbc0fd434dfc30ca252ff69a491ed90af0e
Codetransform <18204099093@163.com>
2021-09-05 00:17:10 +0800

    更新后台页面

---
+++ admin/src/main/java/com/zba/admin/entity/SysUser.java
@@ -0,0 +1,67 @@
+package com.zba.admin.entity;
+
+public class SysUser {
+    private int id;
+    private String username;
+    private String password;
+    private String name;
+    private String idCard;
+    private String birthday;
+    private String sex;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
